9b534da9bf315f1d560baca078d67991954f8dd7
Andrii <uzatsepin@gmail.com>
2022-01-28 14:11:13 +0200

    js file mod

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-"use strict";
+'use strict';
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const answer = prompt('Вам есть 18?', 'Нет');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-const answers = [];
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt("Один из последних просмотренных фильмов?", ""),
+    d = prompt('На сколько оцените его?', '');
 
-answers[0] = prompt('Как вас зовут?', '');
-answers[1] = +prompt('Сколько вам лет?', '');
-answers[2] = prompt('Как дела?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-document.write(answers);
+console.log(personalMovieDB);
